0f1a90110590780a92190b858d80a4285ff9aabd
Sundram Guha <sundram.guha@yash.com>
2020-07-16 14:37:13 +0530

    Milestone webpart changes

---
+++ src/webparts/milestone/components/getQueryString.ts
@@ -0,0 +1,12 @@
+import  * as $ from 'jquery'
+
+
+export function GetParameterValues(param) {
+    var url = window.location.href.slice(window.location.href.indexOf(',') + 1).split('&');
+    for (var i = 0; i < url.length; i++) {
+        var urlparam = url[i].split('=');
+        if (urlparam[0] == param) {
+            return urlparam[1];
+        }
+    }
+}
