d9fe2611c4fb881d7be33b45922457c7c6eebb0a
Vjoshi22 <vivekjoshisvce@gmail.com>
2020-07-31 14:29:27 +0530

    img added

---
+++ src/webparts/welcomePageWp/components/IWelcomePageWpProps.ts
@@ -0,0 +1,3 @@
+export interface IWelcomePageWpProps {
+  description: string;
+}
---
+++ src/webparts/welcomePageWp/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IWelcomePageWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WelcomePageWpWebPartStrings' {
+  const strings: IWelcomePageWpWebPartStrings;
+  export = strings;
+}
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
