2db3f42fb962257ac911449eba6e6a3565d2ad6c
Vjoshi22 <vivekjoshisvce@gmail.com>
2020-08-12 14:53:32 +0530

    check browser for tile view IE or Not

--- src/webparts/tileNavigaitonPmo/TileNavigaitonPmoWebPart.ts
+++ src/webparts/tileNavigaitonPmo/TileNavigaitonPmoWebPart.ts
@@ -20,6 +20,7 @@ import {
   PropertyFieldListPicker,
   PropertyFieldListPickerOrderBy,
 } from "@pnp/spfx-property-controls/lib/PropertyFieldListPicker";
+import CheckBrowser from "../../checkBrowser";
 
 export interface ITileNavigaitonPmoWebPartProps {
   description: string;
@@ -27,6 +28,7 @@ export interface ITileNavigaitonPmoWebPartProps {
   lists: string | string[];
   tileName: string;
 }
+var renderTiles: any;
 
 export default class TileNavigaitonPmoWebPart extends BaseClientSideWebPart<
   ITileNavigaitonPmoWebPartProps
@@ -35,8 +37,18 @@ export default class TileNavigaitonPmoWebPart extends BaseClientSideWebPart<
   private parentItemDropdownOptions: IPropertyPaneDropdownOption[] = [];
 
   public render(): void {
+    let userAgentString = navigator.userAgent;
+    let IExplorerAgent =
+      userAgentString.indexOf("MSIE") > -1 ||
+      userAgentString.indexOf("rv:") > -1;
+    //checking the current browser is IE, if IE then asking the user to use modern browsers
+    if (IExplorerAgent) {
+      renderTiles = CheckBrowser;
+    } else {
+      renderTiles = TileNavigaitonPmo;
+    }
     const element: React.ReactElement<ITileNavigaitonPmoProps> = React.createElement(
-      TileNavigaitonPmo,
+      renderTiles,
       {
         description: this.properties.description,
         currentContext: this.context,
